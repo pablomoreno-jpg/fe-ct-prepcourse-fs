6caad1e5573c69144ac2abf5c43a13b3b376ffa9
pablo moreno <pablommoreno5@gmail.com>
2023-10-26 17:28:31 -0300

    modeulo 3: ejercios 1 al 4

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,46 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+
+   console(x == y);
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+
+   console.log(str1.length == str2.length);
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   console.log(num < 90);
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+
+   console.log(num > 50);
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   console.log(num % 2 == 0)
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+
+   console.log(num % 2 != 0)
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
@@ -46,3 +56,7 @@ module.exports = {
    esPar,
    esImpar,
 };
+
+
+esImpar(2)
+
--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,37 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   console.log(num ** 4);
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   console.log(num ** 3);
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   console.log(num ** exponent);
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   console.log(Math.ceil(num))
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   console.log(redondearNumero(Math.random() * 1));
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
@@ -42,3 +48,5 @@ module.exports = {
    redondearHaciaArriba,
    numeroRandom,
 };
+
+numeroRandom()
