da1624c60002bfb51239d9d0d4f7afb532112ef7
pablo moreno <pablommoreno5@gmail.com>
2023-10-31 15:46:01 -0300

    primera parte del modulo 7

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,54 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+
+   class Usuario {
+
+      constructor(usuario,nombre,email,password){
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+
+      saludar(){
+
+         return "Hola, mi nombre es " + this.nombre;
+      }
+   }
+
+   return Usuario;
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+
+   Usuario.prototype.saludar = function(){ return "Hello World!"};
+   
+   return Usuario;
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+
+   String.prototype.reverse = function(){
+
+      var arraycadena = this.split('');
+      var arrayinvertido = []; 
+
+      for(var i = arraycadena.length; i > -1; i--){
+
+         arrayinvertido.push(arraycadena[i]);
+      }
+
+      var reverso = arrayinvertido.join('')
+
+      return reverso;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
@@ -30,3 +66,5 @@ module.exports = {
    agregarMetodoPrototype,
    agregarStringInvertida,
 };
+
+
